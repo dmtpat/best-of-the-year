416f39a365aed9d5f05681df66d1333e5bfd3efe
Pat desktop <dmtpatricia@gmail.com>
2026-05-28 18:49:12 +0200

    Step 1

---
+++ src/main/java/org/lessons/java/best_of_the_year/controller/homeController.java
@@ -0,0 +1,18 @@
+package org.lessons.java.best_of_the_year.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+    
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("io", new String("Patricia"));
+        return "home";
+    }
+
+}
